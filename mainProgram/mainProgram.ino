e123371a4485fe5c84c5d683c64c04c87289d620
tonytiratay <tony.tiratay@gmail.com>
2020-05-02 19:50:25 +0200

    use ButtonCtrl library to handle press and long press <3

--- mainProgram/mainProgram.ino
+++ mainProgram/mainProgram.ino
@@ -1,9 +1,26 @@
+# include <ButtonCtrl.h>
+
+ButtonCtrl mainButton(8);
+int _modes = 1;
+int _mode = 0;
+
+void handlePress()
+{
+  Serial.println("Pressed");
+}
+
+void handleLongPress()
+{
+  Serial.println("Long Pressed !");
+}
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  mainButton.onPress(handlePress);
+  mainButton.onLongPress(handleLongPress);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  mainButton.loop();
 }
